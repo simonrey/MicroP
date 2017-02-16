ac6e87da6f89b44a787bef43e4c4c4bbef3f9c47
Simon <Simon>
2017-02-15 22:20:25 -0500

    Got rid of bars

--- Lab2_STM32F4Cube_Base_project/LAB2/Src/main.c
+++ Lab2_STM32F4Cube_Base_project/LAB2/Src/main.c
@@ -343,7 +343,7 @@ void GPIO_Setup(void){
 	HAL_GPIO_Init(GPIOA, &init_gpio);
 	
 	//Output on GPIOB -- Need 13 pins, just decided to hit all of them on for now
-	init_gpio.Pin = ((GPIO_PIN_4 || GPIO_PIN_5 || GPIO_PIN_6 || GPIO_PIN_7 || GPIO_PIN_8 || GPIO_PIN_9 || GPIO_PIN_10 || GPIO_PIN_11 || GPIO_PIN_12 || GPIO_PIN_13 || GPIO_PIN_14 || GPIO_PIN_15));
+	init_gpio.Pin = ((GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6  | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15));
 	init_gpio.Mode = GPIO_MODE_OUTPUT_PP;
 	init_gpio.Pull = GPIO_PULLDOWN;
 	init_gpio.Speed = GPIO_SPEED_FREQ_MEDIUM;
